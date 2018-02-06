17199ea318ffe65827b7b92a3c2b3ac91ae021a6
Dinko[cURL] <dinkochess@gmail.com>
2018-02-06 22:50:28 +0100

    99% done

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var pg = require("pg");
 
 // Database setup connection
 var username = 'postgres'; // sandbox username
-var password = 'postgresrut'; // read only privileges on our table
+var password = 'postgres'; // read only privileges on our table
 var host = 'localhost';
 var database = 'hr_2po_4pgr'; // database name
 var databaseConnection = "postgres://" + username + ":" + password + "@" + host + "/" + database; // Your Database Connection
@@ -63,47 +63,70 @@ query.on("end", function (result) {
 /* GET the map page */
 router.get('/map', function (req, res) {
 
-    res.render('map', {
-        title: 'GIS App'
-    });
+    var names = 'select row_to_json(hrv) from (select id, name from mlin_points) hrv';
 
-    /*var client = new pg.Client(databaseConnection); // Setup our Postgres Client
-    client.connect(); // connect to the client
-    var query = client.query(croatia_roads); // Run our Query
+    var client = new pg.Client(databaseConnection);
+    client.connect();
+    var query = client.query(names);
     query.on("row", function (row, result) {
         result.addRow(row);
     });
-    // Pass the result to the map page
     query.on("end", function (result) {
-        var data = result.rows[0].row_to_json; // Save the JSON as variable data
         res.render('map', {
-            title: "GIS App", // Give a title to our page
-            jsonData: result // Pass data to the View
+            title: 'GIS App',
+            jsonData: result
         });
-    });*/
+    });
+
+    /*
+        var data = result.rows[0].row_to_json; // Save the JSON as variable data
+    */
 });
 
 router.post('/getData', function (req, res) {
     var dataFromClient = req.body;
 
-    var source_lat = dataFromClient.data.source_lat;
+    var source_id = dataFromClient.data.idSource;
+    var destination_id = dataFromClient.data.idDestination;
+    var algorithm = dataFromClient.data.path;
+
+    var mlin_query_shortest = 'select row_to_json(hrv) from (SELECT pt.seq, pt.node, pt.edge, pt.cost, pt.agg_cost, st_x(st_startpoint(rd.geom)) as src_x, st_y(st_startpoint(rd.geom)) as src_y, st_x(st_endpoint(rd.geom)) as tg_x, st_y(st_endpoint(rd.geom)) as tg_y FROM pgr_dijkstra(\'SELECT id, source, target, km as cost FROM mlin_lines\', ' + source_id + ' , ' + destination_id + ') as pt JOIN mlin_lines rd ON pt.edge = rd.id) hrv';
+    var mlin_query_fastest = 'select row_to_json(hrv) from (SELECT pt.seq, pt.node, pt.edge, pt.cost, pt.agg_cost, st_x(st_startpoint(rd.geom)) as src_x, st_y(st_startpoint(rd.geom)) as src_y, st_x(st_endpoint(rd.geom)) as tg_x, st_y(st_endpoint(rd.geom)) as tg_y FROM pgr_dijkstra(\'SELECT id, source, target, km/kmh as cost FROM mlin_lines\', ' + source_id + ' , ' + destination_id + ') as pt JOIN mlin_lines rd ON pt.edge = rd.id) hrv';
+
+
+    /*var source_lat = dataFromClient.data.source_lat;
     var source_lng = dataFromClient.data.source_lng;
     var dest_lat = dataFromClient.data.dest_lat;
-    var dest_lng = dataFromClient.data.dest_lng;
+    var dest_lng = dataFromClient.data.dest_lng;*/
 
-    var croatia_roads = 'select row_to_json(hrv) from (SELECT pt.seq, pt.node, pt.edge, pt.cost, pt.agg_cost, rd.km, rd.kmh, rd.km/rd.kmh as vrijeme, rd.x1 as x1, rd.x2 as x2, rd.y1 as y1, rd.y2 as y2, rd.geom_way as geom FROM pgr_dijkstra(\'SELECT id, source, target, cost FROM hr_2po_4pgr\',(SELECT source FROM hr_2po_4pgr ORDER BY ST_Distance(ST_StartPoint(geom_way),ST_SetSRID(ST_MakePoint(' + source_lng + ',' + source_lat + '), 4326),true) ASC LIMIT 1),(SELECT source FROM hr_2po_4pgr ORDER BY ST_Distance(ST_StartPoint(geom_way),ST_SetSRID(ST_MakePoint(' + dest_lng + ',' + dest_lat + '), 4326),true) ASC LIMIT 1)) as pt JOIN hr_2po_4pgr rd ON pt.edge = rd.id) hrv';
+    //var croatia_roads = 'select row_to_json(hrv) from (SELECT pt.seq, pt.node, pt.edge, pt.cost, pt.agg_cost, rd.km, rd.kmh, rd.km/rd.kmh as vrijeme, rd.x1 as x1, rd.x2 as x2, rd.y1 as y1, rd.y2 as y2, rd.geom_way as geom FROM pgr_dijkstra(\'SELECT id, source, target, cost FROM croatia_roads\',(SELECT source FROM croatia_roads ORDER BY ST_Distance(ST_StartPoint(geom_way),ST_SetSRID(ST_MakePoint(' + source_lng + ',' + source_lat + '), 4326),true) ASC LIMIT 1),(SELECT source FROM croatia_roads ORDER BY ST_Distance(ST_StartPoint(geom_way),ST_SetSRID(ST_MakePoint(' + dest_lng + ',' + dest_lat + '), 4326),true) ASC LIMIT 1)) as pt JOIN croatia_roads rd ON pt.edge = rd.id) hrv';
+    if (algorithm === 'shortestPath') {
+         var client = new pg.Client(databaseConnection);
+        client.connect();
+        var query = client.query(mlin_query_shortest);
 
-    var client = new pg.Client(databaseConnection);
-    client.connect();
-    var query = client.query(croatia_roads);
+        query.on("row", function (row, result) {
+            result.addRow(row);
+        });
+        query.on("end", function (result) {
+            //var data = result.rows[0].row_to_json;
+            res.status(200).send(result);
+        });
+    }
+    if (algorithm === 'fastestPath') {
+         var client = new pg.Client(databaseConnection);
+        client.connect();
+        var query = client.query(mlin_query_fastest);
 
-    query.on("row", function (row, result) {
-        result.addRow(row);
-    });
-    query.on("end", function (result) {
-        //var data = result.rows[0].row_to_json;
-        res.status(200).send(result);
-    });
+        query.on("row", function (row, result) {
+            result.addRow(row);
+        });
+        query.on("end", function (result) {
+            //var data = result.rows[0].row_to_json;
+            res.status(200).send(result);
+        });
+    }
+   
 });
 
 module.exports = router;
